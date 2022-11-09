17f6fbea3e7985feec8eda3edb914999dd886f6d
v.plaksunova <majtyan@gmail.com>
2022-11-09 18:49:57 +0300

    add configs

---
+++ sync-hack/src/main/java/synchack/financial.market/SyncHackApplication.java
@@ -0,0 +1,13 @@
+package synchack.financialmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncHackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncHackApplication.class, args);
+	}
+
+}
