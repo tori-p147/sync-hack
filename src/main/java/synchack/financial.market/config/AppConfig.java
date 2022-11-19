95f405ac1827e8daa45f34ea18cceeb69f5a1896
v.plaksunova <majtyan@gmail.com>
2022-11-19 19:58:28 +0300

    fix bean

--- src/main/java/synchack/financial.market/config/AppConfig.java
+++ src/main/java/synchack/financial.market/config/AppConfig.java
@@ -2,11 +2,17 @@ package synchack.financial.market.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @Slf4j
 @EnableCaching
 public class AppConfig {
-
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
