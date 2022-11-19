c4143636e4d2d6439c73951a7067c3745e94ed0f
v.plaksunova <majtyan@gmail.com>
2022-11-19 19:33:03 +0300

    liqui configure

--- src/main/java/synchack/financial.market/config/AppConfig.java
+++ src/main/java/synchack/financial.market/config/AppConfig.java
@@ -1,11 +1,8 @@
 package synchack.financial.market.config;
 
-import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 @Slf4j
